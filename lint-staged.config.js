cdcedf5ab9365d3a1901521e523674016f17426c
Isai Lopez Rodas <40477553+ilopezro@users.noreply.github.com>
2025-03-08 16:20:33 -0800

    chore: remove yarn, add npm

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   "**/*.ts": (files) => {
-    return files.map((file) => `yarn eslint ${file} --fix`);
+    return files.map((file) => `npx eslint ${file} --fix`);
   },
   "**/*.tsx": (files) => {
-    return files.map((file) => `yarn eslint ${file} --fix`);
+    return files.map((file) => `npx eslint ${file} --fix`);
   },
   "*/**.scss": (files) => {
-    return files.map((file) => `yarn stylelint ${file} --fix`);
+    return files.map((file) => `npx stylelint ${file} --fix`);
   },
 };
